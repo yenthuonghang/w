b917ab2c2f6bcd24a47b52de45fafa4f1a6cf09b
yenthuonghang <134287013+yenthuonghang@users.noreply.github.com>
2023-05-22 20:48:24 +0700

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+AOS.init({
+    duration: 800,
+    easing: 'ease-in-out',
+    once: false,
+    mirror: false
+});
+
+const lightbox = GLightbox({
+    openEffect: 'zoom',
+    closeEffect: 'fade',
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true,
+
+});
